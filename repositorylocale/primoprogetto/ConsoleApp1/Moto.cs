15d046447214bc8967a2fb349361f9613169bd40
Pizfnnellabomba <giuosesto@gmail.com>
2025-10-22 09:35:16 +0200

    Moto.cs

---
+++ repositorylocale/primoprogetto/ConsoleApp1/Moto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Moto
+    {
+        private Veicolo veicoli;
+        private string marca;
+        private string modello;
+        private string colore;
+        public Moto(string marca, string modello, string colore)
+        {
+            this.marca = marca;
+            this.modello = modello;
+            this.colore = colore;
+        }
+    }
+}
